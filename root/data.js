7129dbc9ab6f4707119d0e5f70b213f78fe68f0c
RustPolaris <81266127+RustPolaris@users.noreply.github.com>
2021-05-19 20:07:46 -0300

    Add files via upload

---
+++ root/data.js
@@ -0,0 +1,54 @@
+var data = {
+    games_data:
+        [
+            {
+                title: "Hades",
+                genre: ['Roguelite','All'],
+                dev: "Supergiant Games",
+                image: 'placeholder-jogos.png',
+            }, {
+                title: "Risk Of Rain 2",
+                genre: ["Roguelite",'All'],
+                dev: ["Hopoo Games", "PlayEveryWare"],
+                image: 'placeholder-jogos.png',
+            }, {
+                title: "Realpolitiks 2",
+                genre: ["Grand-Strategy",'All'],
+                dev: "Jujubee",
+                image: 'placeholder-jogos.png',
+            }, {
+                title: "Hero Siege",
+                genre: ["RPG",'All'],
+                dev: ["Panic Art Studios", "Elias Viglione", "Jussi Kukkonen"],
+                image: 'placeholder-jogos.png',
+            }, {
+                title: "Stardew Valley",
+                genre: ["Simulation",'All'],
+                dev: ["Eric Barone", "Sickhead Games", "ConcernedApe"],
+                image: 'placeholder-jogos.png',
+            }, {
+                title: "Paralives",
+                genre: ["Simulation",'All'],
+                dev: "Alex Massé",
+                image: 'placeholder-jogos.png',
+            }, {
+                title: "TaleSpire",
+                genre: ["Tabletop",'All'],
+                dev: "Bouncyrock Entertainment",
+                image: 'placeholder-jogos.png',
+            }, {
+                title: "SUPERHOT",
+                genre: ["Action",'All'],
+                dev: "SUPERHOT Team",
+                image: 'placeholder-jogos.png',
+            },{
+                title: "Shadowrun Returns",
+                genre: ["CRPG",'All'],
+                dev: "Harebrained Schemes",
+                image: 'placeholder-jogos.png',
+            },
+        ]
+}
+
+
+
