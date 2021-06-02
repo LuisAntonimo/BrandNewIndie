482d9f88d7967ebff450c2b7b3e4ad51d0ef1182
Luís Antônio <luisantoniossouza@yahoo.com.br>
2021-06-02 10:53:47 -0300

    refactor: index.html, styles.css and data.js

--- Codigo/root/js/data.js
+++ Codigo/root/js/data.js
@@ -3,49 +3,49 @@ var data = {
         [
             {
                 title: '<a style="text-decoration:none" href="https://store.steampowered.com/app/1145360/Hades/">Hades</a>',
-                genre: ['Roguelite','All'],
+                tags: ['Roguelite','All'],
                 dev: "Supergiant Games",
-                image: './images/placeholder-jogos.png',
+                image: 'https://placehold.jp/264x352.png',
             }, {
                 title: '<a style="text-decoration:none" href="https://store.steampowered.com/app/632360/Risk_of_Rain_2/">Risk Of Rain 2</a>',
-                genre: ["Roguelite",'All'],
+                tags: ["Roguelite",'All'],
                 dev: ["Hopoo Games", "PlayEveryWare"],
-                image: './images/placeholder-jogos.png',
+                image: 'https://placehold.jp/264x352.png',
             }, {
                 title: '<a style="text-decoration:none" href="https://store.steampowered.com/app/1248060/Realpolitiks_II/">Realpolitiks 2</a>',
-                genre: ["Grand-Strategy",'All'],
+                tags: ["Grand-Strategy",'All'],
                 dev: "Jujubee",
-                image: './images/placeholder-jogos.png',
+                image: 'https://placehold.jp/264x352.png',
             }, {
                 title: '<a style="text-decoration:none" href="https://store.steampowered.com/app/269210/Hero_Siege/">Hero Siege</a>',
-                genre: ["RPG",'All'],
+                tags: ["RPG",'All'],
                 dev: ["Panic Art Studios", "Elias Viglione", "Jussi Kukkonen"],
-                image: './images/placeholder-jogos.png',
+                image: 'https://placehold.jp/264x352.png',
             }, {
                 title: '<a style="text-decoration:none" href="https://store.steampowered.com/app/413150/Stardew_Valley/>Stardew Valley</a>',
-                genre: ["Simulation",'All'],
+                tags: ["Simulation",'All'],
                 dev: ["Eric Barone", "Sickhead Games", "ConcernedApe"],
-                image: './images/placeholder-jogos.png',
+                image: 'https://placehold.jp/264x352.png',
             }, {
                 title: '<a style="text-decoration:none" href="https://store.steampowered.com/app/1118520/Paralives/">Paralives</a>',
-                genre: ["Simulation",'All'],
+                tags: ["Simulation",'All'],
                 dev: "Alex Massé",
-                image: './images/placeholder-jogos.png',
+                image: 'https://placehold.jp/264x352.png',
             }, {
                 title: '<a style="text-decoration:none" href="https://store.steampowered.com/app/720620/TaleSpire/">TaleSpire</a>',
-                genre: ["Tabletop",'All'],
+                tags: ["Tabletop",'All'],
                 dev: "Bouncyrock Entertainment",
-                image: './images/placeholder-jogos.png',
+                image: 'https://placehold.jp/264x352.png',
             }, {
                 title: '<a style="text-decoration:none" href="https://store.steampowered.com/app/322500/SUPERHOT/">SUPERHOT</a>',
-                genre: ["Action",'All'],
+                tags: ["Action",'All'],
                 dev: "SUPERHOT Team",
-                image: './images/placeholder-jogos.png',
+                image: 'https://placehold.jp/264x352.png',
             },{
                 title: '<a style="text-decoration:none". href="https://store.steampowered.com/app/234650/Shadowrun_Returns/">Shadowrun Returns</a>',
-                genre: ["CRPG",'All'],
+                tags: ["CRPG",'All'],
                 dev: "Harebrained Schemes",
-                image: './images/placeholder-jogos.png',
+                image: 'https://placehold.jp/264x352.png',
             },
         ]
 }
