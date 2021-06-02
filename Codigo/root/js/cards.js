2ffe6f0f2892b595b1a33fdb22e1bd122aa4b8b4
Luís Antônio <luisantoniossouza@yahoo.com.br>
2021-06-02 15:30:14 -0300

    refactor: card system

---
+++ Codigo/root/js/cards.js
@@ -0,0 +1,17 @@
+import data from './data.js' // importa o conteúdo de data.js
+
+const gameList = document.getElementById('games-list');
+
+function createCard(game) { // cria um card baseado nos dados tirados do parâmetro
+  const card = document.createElement('div');
+
+  card.className = 'col-12 col-sm-12 col-md-3 col-lg-3 card-transparent';
+
+  card.innerHTML = `<img src="${game.image}" class="card-img-top" alt="${game.title}">`;
+
+  gameList.appendChild(card); // adiciona o card a games-list
+}
+
+for (let i = 0; i < data.length; i++) { // cria os cards com base em cada item de data.js
+  createCard(data[i]);
+}
