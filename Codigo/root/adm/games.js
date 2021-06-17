18353adff2e5d0595fc8d23716a6270bceb11078
Luís Antônio <luisantoniossouza@yahoo.com.br>
2021-06-17 19:29:49 -0300

    feat: cadastro de jogos

--- Codigo/root/adm/games.js
+++ Codigo/root/adm/games.js
@@ -1,32 +1,69 @@
-import data from '../js/data.js';
+let game_db = {};
+
+const dadosIniciais = {
+	game_list: []
+}
+
+function init() {
+	const gameJSON = localStorage.getItem('game_db');
+
+    if (!gameJSON) {   
+        game_db = dadosIniciais;
+        localStorage.setItem('game_db', JSON.stringify (dadosIniciais));
+    }
+    else  {
+        game_db = JSON.parse(gameJSON);    
+    }
+}
+
 
 const button = document.getElementById('btt');
-button.addEventListener('click', teste)
+button.addEventListener('click', searchGames)
 
-const url = `https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games`;
+const url = 'https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games';
 const myHeaders = new Headers();
 myHeaders.append('Client-ID', 'rnxg276wty5wu058cirpt702s7ry4c');
 myHeaders.append('Authorization','Bearer vpg2qovzjnzizgjwhl289fr9tchzla');
-let body;
 
 
-function teste() {
+function searchGames() {
+	init();
 	const query = document.getElementById('search').value
-	body = `fields name cover screenshots videos involved_companies; search "${query}"; where version_parent = null;`
+
+	const body = `fields name, cover, screenshots, videos, involved_companies; search "${query}"; where version_parent = null; limit 1;`
+
 	const myInit = { method: 'POST', body: body , headers: myHeaders};
+
 	fetch(url, myInit)
 	.then(response => response.json())
 	.then(data => {
-		for (let i = 0; i < data.length; i++) {
-			const game = {
-				'name': data[i].name,
-				'media': {
-					"cover": data[i].cover,
-					"screenshots": data[i].screenshots,
-					"videos": data[i].videos,
-				},
-				'involved_companies': data[i].involved_companies
+			for (let i = 0; i < data.length; i++) {
+					const game = {
+							'name': data[i].name,
+							'media': {
+									'cover': data[i].cover,
+									'screenshots': data[i].screenshots,
+									'videos': data[i].videos,
+							},
+							'involved_companies': data[i].involved_companies
+					}
+					cadastrarGame(game);
 			}
-		}
 	})
+}
+
+function cadastrarGame(gameData) {
+	const gameObject = {
+			'Id': '',
+			'title': `${gameData.name}`,
+			'tags': [],
+			'dev': gameData.involved_companies,
+			'media': {
+					'cover': gameData.media.cover,
+					'screenshots': gameData.media.screenshots,
+					'videos': gameData.media.videos,
+			}
+	}
+	game_db.game_list.push(gameObject);
+	localStorage.setItem('game_db', JSON.stringify (game_db));
 }
